4e3def51ffa771e4fa0270f9d15feda4e0e1933f
gorbacenkovitalij6-prog <gorbacenkovitalij6@gmail.com>
2026-03-25 11:48:55 +0200

    fix: Add redirect for old address URL to resolve 404

--- next.config.js
+++ next.config.js
@@ -26,6 +26,20 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/maps/240/togliatti/house/ulitsa_marshala_zhukova_54/:path*',
+        destination: '/maps/240/togliatti/house/shlyuzovaya_ul_2/:path*',
+        permanent: true,
+      },
+      {
+        source: '/maps/240/togliatti/house/ulitsa_marshala_zhukova_54',
+        destination: '/maps/240/togliatti/house/shlyuzovaya_ul_2/YEAYdA5oTEQHQFtpfXlxcnhgbQ==',
+        permanent: true,
+      }
+    ];
+  },
 };
 
 module.exports = nextConfig;
